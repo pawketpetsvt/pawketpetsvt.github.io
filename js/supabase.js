9c04cc06df47d6b6b2d7fa23a4c22e979791be85
Ry <OfficiallyRy@outlook.com>
2026-08-26 04:06:52 -0400

    Phase 6.5 and Phase 7 both done. Huge Bootstrap integration overhaul for Phase 6.5 and very large process of implementing the Battle system (and fixing many styles in between)

--- js/supabase.js
+++
@@ -1,5 +0,0 @@
-const SUPABASE_URL = 'https://hqzugbxutgefjilgmxqu.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_RRdq-futHfTDJoQgFVLvTQ_gie9dLmd';
-
-const { createClient } = supabase;
-const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
